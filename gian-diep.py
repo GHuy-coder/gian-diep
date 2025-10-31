1ba81260cf52161fde7b42a130785d6def2d1b22
GiaHuy <giahuyluong609@gmail.com>
2025-10-31 20:50:39 +0700

    gian-diep.fix

--- gian-diep.py
+++ gian-diep.py
@@ -6,10 +6,11 @@ text = Text(box_text, "Gián điệp đang nằm ở đây?", size=22, color="bl
 box = Box(app, width=546, height=540, layout="grid", border=True)
 x= 0
 y = 0
+limit = 5
 ranx = randint(0, 2)
 rany = randint(0, 2)
 def gian_diep(z, c, n):
-    global ranx, rany
+    global ranx, rany, limit
     if z == ranx and c == rany:
         n.text = ""
         n.image = "roland.png"
@@ -17,13 +18,17 @@ def gian_diep(z, c, n):
         n.height = 173
         n.bg="#696969"
         info("thong bao", "Chúc mừng bạn đã tìm được gián điệp")
-        n.disable()
+        app.destroy()
     else:
         info("thong bao", "Ôi no, không phải là vị trí của gián điệp")
         n.image = "sai.jpg"
         n.width = 176
         n.height = 173
         n.disable()
+        limit -= 1
+        if limit <= 0:
+            info("thong bao", "bạn đã để đủ thời gian cho gián điệp trốn thoát")
+            app.destroy()
 for i in range(3):
     for j in range(3):
         push = PushButton(box, image="invite.jpg" ,width=176, height=173, grid=[i, j], command= gian_diep, args=[i, j])
